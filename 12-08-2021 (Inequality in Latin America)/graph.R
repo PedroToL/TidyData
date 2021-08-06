da16c3755e8c05be67a69cafe4fac5733e88f21e
Pedro Torres <66799113+PedroToL@users.noreply.github.com>
2021-08-05 22:32:25 -0500

    Added graphs for 12-08-2021

---
+++ 12-08-2021 (Inequality in Latin America)/graph.R
@@ -0,0 +1,46 @@
+# Libraries ----
+library(tidyverse)
+library(plotly)
+
+# Data
+df <- read_csv("./LatinAmerica/Data/Df.csv")
+
+# Graph
+fig <- df %>% filter(year == 2018, region != "-") %>%
+  plot_ly(x     = ~GDP_PC_2010,
+          y     = ~ratio, 
+          type  = 'scatter',
+          mode  = 'markers',
+          color = ~region,
+          marker = list(size    = ~density,
+                        line    = list(width = 1.2, color = 'rgb(300, 300, 300)'),
+                        sizeref = 4,
+                        sizemin = 6,
+                        opacity = 0.5),
+          colors  = c('#2D49BA', '#57AB3C', '#BB3754FF')
+  ) 
+
+%>% add_trace(
+    text = ~c_name,
+    textposition = "top",
+    textfont = list(color = '#000000', size = 10),
+    showlegend = F
+  )
+
+fig <- fig %>% layout(title  = 'Relation between GDP per Capita,People living in Urban\nRural areas and Population density (Size)\n2018',
+                      xaxis  = list(title= "GDP per Capita (Constant US$ 2010)",
+                                    gridcolor = 'rgb(215, 215, 215)'),
+                      yaxis  = list(title = "Urban/Rural Ratio %",
+                                    gridcolor = 'rgb(215, 215, 215)'),
+                      margin = list(l = 75, r=20, t=75, b=75),
+                      paper_bgcolor = 'rgb(200, 200, 200)',
+                      plot_bgcolor  = 'rgb(200, 200, 200)',
+                      legend = list(title = list(text = '<b> Region <b>'), x = 0.04, y = .96,
+                                    bgcolor = "#E2E2E2",
+                                    bordercolor = "#FFFFFF",
+                                    borderwidth = 2)
+  )
+
+fig
+
+  
