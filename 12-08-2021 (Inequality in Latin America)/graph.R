e7bc01023c9b595598748823eb0ed2907d86b2bd
Pedro Torres <66799113+PedroToL@users.noreply.github.com>
2021-08-10 17:29:48 -0500

    Changed Font-Size

--- 12-08-2021 (Inequality in Latin America)/graph.R
+++ 12-08-2021 (Inequality in Latin America)/graph.R
@@ -27,24 +27,30 @@ fig <- df %>% filter(year == 2018, region != "-") %>%
   ) %>% add_trace(
     text = ~c_name,
     textposition = "top",
-    textfont = list(color = '#000000', size = 10),
-    showlegend = F
+    textfont     = list(color = '#000000', 
+                        size = 12),
+    showlegend   = F
   )
 
-fig <- fig %>% layout(title  = 'Relation between GDP per Capita,People living in Urban\nRural areas and Population density (Size)\n2018',
+fig <- fig %>% layout(title  = '<b>Relation between GDP per Capita,People living in Urban\nRural areas and Population density (Size)\n2018<b>',
+                      font   = list(size = 18),
                       xaxis  = list(title= "GDP per Capita (Constant US$ 2010)",
-                                    gridcolor = 'rgb(215, 215, 215)'),
+                                    gridcolor = 'rgb(215, 215, 215)', 
+                                    tickfont  = list(size = 15),
+                                    titlefont = list(size = 18)),
                       yaxis  = list(title = "Urban/Rural Ratio %",
-                                    gridcolor = 'rgb(215, 215, 215)'),
+                                    gridcolor = 'rgb(215, 215, 215)', 
+                                    tickfont  = list(size = 15),
+                                    titlefont = list(size = 18)),
                       margin = list(l = 75, r=20, t=75, b=75),
                       paper_bgcolor = 'rgb(200, 200, 200)',
                       plot_bgcolor  = 'rgb(200, 200, 200)',
-                      legend        = list(title        = list(text = '<b> Region <b>'),
+                      legend        = list(title       = list(text = '<b> Region <b>'),
                                            x = 0.04, y = .96,
                                            bgcolor     = "#E2E2E2",
                                            bordercolor = "#FFFFFF",
                                            borderwidth = 2),
-                      annotations  = list(x = 1, y = -0.12, text = "Source: World Bank Open Data", 
+                      annotations  = list(x = 1, y = -0.17, text = "Source: World Bank Open Data", 
                                           showarrow = F, xref='paper', yref='paper', 
                                           xanchor='right', yanchor='auto', xshift=0, yshift=0,
                                           font=list(size=15, color="'rgb(100, 100, 100)"))
